c2bb7f0fd0b06bf5cc2720d56c7eac2eff1d64ed
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-18 16:42:26 -0500

    Elvis. HStack Button Actions to change image and text in the outer VStack. Main viewing area

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    // blank to start
+    @State private var imagetext = ""
+    @State private var systemNameImage = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny \n'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+              Image(systemName: systemNameImage)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+            Text(imagetext)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            HStack {
+                Button("Peace") {
+                    imagetext = "Peace"
+                    systemNameImage = "peacesign"
+                }
+                Button("Love") {
+                    imagetext = "Love"
+                    systemNameImage = "heart"
+                }
+                Button("Understanding") {
+                    imagetext = "Understanding"
+                    systemNameImage = "lightbulb"
+
+                }
+            }
+            
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
-        .padding()
+      .padding()
     }
 }
 
